8d9f1110861117793ef7de0f2a3d0e9d2fad0e7b
Tenzin <tenzin.thakuri01@gmail.com>
2026-02-09 10:33:19 -0500

    feat:implemented CRUD on JOB, error validation

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 require("dotenv").config();
 require("express-async-errors");
+//extra security packages
+const helmet = require("helmet");
+const cors = require("cors");
+const xss = require("xss-clean");
+const rateLimiter = require("express-rate-limit");
 const express = require("express");
 const app = express();
 //connect DB
@@ -14,6 +19,11 @@ const errorHandlerMiddleware = require("./middleware/error-handler");
 
 app.use(express.json());
 // extra packages
+app.set("trust proxy", 1);
+app.use(rateLimiter({ windowMs: 15 * 60 * 1000, max: 100 }));
+app.use(helmet());
+app.use(cors());
+app.use(xss());
 
 // routes
 app.use("/api/v1/auth", authRouter);
